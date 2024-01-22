492fb028cb70e3730613e677bc5d4e0caed439cd
LukeB <luke.burnett@gsa.gov>
2024-01-22 12:06:45 -0500

    Edtied 21.2-ic-dna-3 testfile.
    - Added play button on top of the GIF (Disappear when the user clicks on the GIF).
    - Added a paragraph that describe the GIF image.

--- testfiles/TF21/TF21.2.js
+++ testfiles/TF21/TF21.2.js
@@ -5,7 +5,9 @@ let gifStatic, gifVideo;
 //This function toggle between the image GIF or the animate GIF.
 function toggleGif() {
     let gif = document.getElementById('animated-gif');
+    let overlay = document.getElementById("gif-overlay");
     gif.src = (isPlaying) ? gifStatic : gifVideo;
+    overlay.style.display = (isPlaying) ? "flex" : "none";
     isPlaying = !isPlaying;
 }
 
