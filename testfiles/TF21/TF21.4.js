297383407a9d1c17c5148ff7d412e47bf47db19f
LukeB <luke.burnett@gsa.gov>
2024-01-29 11:11:28 -0500

    Created 21.4-1-fail-2, 21.4-1-fail-3, and 21.4-ic-dna-2 testfiles.

--- testfiles/TF21/TF21.4.js
+++ testfiles/TF21/TF21.4.js
@@ -1,22 +1,40 @@
+let isPlaying = true;
+let audioPlayer;
 window.onload = function ()
 {
-    
+    audioPlayer = document.getElementById("audio-p");
 };
 //This function hide the overlay and start the audio.
 function startAudio()
 {
     document.getElementById("site-access-overlay").style.display = "none";
     document.getElementById("site-access").style.display = "none";
-    document.getElementById("audio-p").play();
+    audioPlayer.play();
 
 }
 //This function stop audio and hide audio player along with the stop button.
 function stopAudio()
 {
-    let audioPlayer = document.getElementById("audio-p");
     audioPlayer.pause();
     audioPlayer.currentTime = 0;
     audioPlayer.style.display = "none";
     document.getElementById("audio-stop").style.display = "none";
 
+}
+//This function toggle between stop or playing the audio on the page, and update the button that click to run this function accordingly.
+function stopResumeAudio(el)
+{
+    if(isPlaying)
+    {
+        audioPlayer.pause();
+        isPlaying = false;
+        el.innerHTML = "Resume audio";
+    }
+    else
+    {
+        audioPlayer.play();
+        isPlaying = true;
+        el.innerHTML = "Pause audio";
+    }
+    
 }
