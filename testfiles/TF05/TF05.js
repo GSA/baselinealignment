c78e38498e1ae5f969c9d792044e1a6f4f084df3
LukeB <luke.burnett@gsa.gov>
2024-02-09 10:18:36 -0500

    Created 05.4-1-fail-1, 05.4-1-pass-1, 05.4-3-fail-1, and 05.4-3-pass-1 testfiles.

--- testfiles/TF05/TF05.js
+++ testfiles/TF05/TF05.js
@@ -1,4 +1,5 @@
 let submitText = "", pageContentList;
+let ratingSlider, ratingOutput
 function submitForm()
 {
     document.getElementsByClassName("page-contents")[0].innerHTML = `<p>${document.getElementById("fname").value} ${document.getElementById("lname").value} your application will be reviewed shortly.</p>`;
@@ -113,6 +114,17 @@ window.onload = function ()
             });
             }
     }
+    else
+    {
+        ratingSlider = document.getElementById("icecream-rating");
+        ratingOutput = document.getElementById("rating");
+        //Does contain Aria-label and function properly.
+        if((document.title == "05.4-1-pass-1")||(document.title == "05.4-3-pass-1")||document.title == "05.4-3-fail-1")
+            updateRating();
+        //Does contain Aria-label but inaccurate.
+        else if(document.title == "05.4-1-fail-1")
+            updateWrongRating();
+    }
 }
 function changePage(page)
 {
@@ -120,4 +132,17 @@ function changePage(page)
     {
         pageContentList[i].style.display = (page == pageContentList[i].id) ? "block" : "none";
     }
+}
+//This function update the rating according to the slider.
+function updateRating()
+{
+    ratingOutput.children[0].innerHTML = `Current rating: ${ratingSlider.value}`;
+    ratingOutput.ariaLabel = `Current rating: ${ratingSlider.value}`
+}
+//This function update the rating according to the slider correclty but not on the aria.
+function updateWrongRating()
+{
+    ratingOutput.children[0].innerHTML = `Current rating: ${ratingSlider.value}`;
+    // ratingSlider.ariaLabel = 11 - ratingSlider.value;
+    ratingOutput.ariaLabel = `Current rating: ${(11 - ratingSlider.value)}`;
 }
