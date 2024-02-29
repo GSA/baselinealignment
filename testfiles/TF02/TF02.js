253da7238371dc9df99ba738547205cbdf476863
LukeB <luke.burnett@gsa.gov>
2024-02-29 14:54:35 -0500

    Created 02.2-ic-dna-2, 02.C-2-fail-1, 02.C-2-fail-2, 02.C-2-fail-3 ,02.C-2-pass-1, 02.C-2-pass-2, and 02.2-ic-dna-1 testfiles

--- testfiles/TF02/TF02.js
+++ testfiles/TF02/TF02.js
@@ -1,4 +1,5 @@
 let helpButton,sideNav,sideNavDisplay = false;
+let inputPopup;
 //Run after all the content loaded.
 document.addEventListener("DOMContentLoaded", function() {
     if((document.title == "02.2-1.a.i-fail-1"))
@@ -6,10 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
         sideNav = document.getElementById("help-sidenav");  
         setSideNavTabIndex(-1);
     }
-    if((document.title =="02.2-1.b.i-fail-1"))
+    if((document.title =="02.2-1.b.i-fail-1") || (document.title =="02.C-2-pass-1"))
     {
         sideNav = document.getElementById("help-sidenav");  
         setSideNavTabIndex(-1);
+    }
+    if((document.title =="02.C-2-fail-3"))
+    {
+        inputPopup = document.getElementsByClassName("input-popup");
+        console.log(inputPopup);
+    }
+    if((document.title == "02.C-2-pass-1"))
+    {
+        var dropdown = document.getElementsByClassName("dropdown-menu");
+        for (let i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("focus", function() 
+            {
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "flex") 
+                {
+                    this.ariaExpanded = "false";
+                    dropdownContent.style.display = "none";
+                } 
+                else 
+                {
+                    this.ariaExpanded = "true";
+                    dropdownContent.style.display = "flex";
+                }
+                this.classList.toggle("active");
+            });
+            }
     }
     
 });
@@ -49,7 +76,31 @@ function toggleSideNav()
 //This fucntion hide/unhide the side nav and set tabIndex for all content inside in order.
 function toggleSideNavOrder()
 {
-   if(sideNavDisplay == false)
+    if((sideNavDisplay == false)&&(document.title =="02.C-2-pass-1"))
+    {
+         sideNavDisplay = true;
+         sideNav.classList.remove("sidenav-hide");
+         sideNav.classList.add("sidenav-display");
+         setFormInputTabIndex(-1);
+         let tabValue = 0;
+         for(let i = 0; i < sideNav.children.length; i++)
+         {
+            if(sideNav.children[i].id != "about-us-dropdown")
+            {
+                tabValue += 1;
+                sideNav.children[i].tabIndex = tabValue;
+            }
+             else
+             {
+                for(let j = 0; j < sideNav.children[i].children.length; j++)
+                {
+                    tabValue += 1;
+                    sideNav.children[i].children[j].tabIndex = tabValue;
+                }
+             }
+         }
+    }
+   else if(sideNavDisplay == false)
    {
         sideNavDisplay = true;
         sideNav.classList.remove("sidenav-hide");
@@ -63,6 +114,15 @@ function toggleSideNavOrder()
             sideNav.children[i].tabIndex = i;
         }
         sideNav.children[1].focus();
+   }
+   else if((sideNavDisplay == true) && ((document.title =="02.C-2-pass-1")))
+   {
+        sideNavDisplay = false;
+        sideNav.classList.remove("sidenav-display");
+        sideNav.classList.add("sidenav-hide");
+        setSideNavTabIndex(-1);
+        setFormInputTabIndex(0);
+        document.getElementById("help").focus();
    }
    else
    {
@@ -95,4 +155,35 @@ function setFormInputTabIndex(value)
 function submitForm()
 {
     document.getElementById("tc_code").innerHTML = "Your form have been submitted!"
+}
+//This function change the page when user select it on the side navigation.
+function changePage(page)
+{
+    let pageContentList = document.getElementById("tc_code").children;
+
+    for(let i = 1; i < pageContentList.length; i++)
+    {
+        pageContentList[i].style.display = (page == pageContentList[i].id) ? "block" : "none";
+    }
+}
+
+//This function click on the element that the agruement recieve.
+function clickLink(el)
+{
+    el.click();
+}
+//This function display the popup for the input.
+function displayPopup(el)
+{
+    const popUp = el.nextElementSibling;
+    if(popUp)
+    {   
+        popUp.classList.remove("popup-hidden");
+        popUp.children[1].focus();
+    }   
+}
+//This function hide the popup when the anchor element on the popup is no longer focus.
+function hidePopup(el)
+{
+    el.parentElement.classList.add("popup-hidden");
 }
