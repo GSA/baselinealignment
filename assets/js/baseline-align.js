c1a0d30817ffd5170be8b1577171753a4d1dce73
Illia Stankevych <illia.stankevych@gsa.gov>
2024-08-15 09:04:04 -0400

    ECASOGP-12830: Fix markup

--- assets/js/baseline-align.js
+++ assets/js/baseline-align.js
@@ -2,7 +2,9 @@
 $(document).ready(function(){
 	$("table")
 		.addClass("usa-table")
-		.addClass("usa-table--borderless");
+		.addClass("usa-table--borderless")
+		.addClass("usa-table--stacked");
+		// .wrap("<div class='usa-table-container--scrollable'></div>");
 	$("table thead th").attr('scope', "col");
 	$("table tbody th").attr('scope', "row");
 });
