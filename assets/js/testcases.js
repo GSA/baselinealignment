8e9ffe36f36091ed289862e4919f07fdb44809c9
Illia Stankevych <illia.stankevych@gsa.gov>
2024-08-15 08:40:00 -0400

    ECASOGP-12830: Update the UI to match the Section508 site

--- assets/js/testcases.js
+++ assets/js/testcases.js
@@ -3,7 +3,7 @@
 //ENTER Test File full URL with http or https.
 
     let file = document.getElementById("samplecodelink").textContent;
-    console.log("File path:" + file);
+    // console.log("File path:" + file);
 
     fetch (file)
     .then(function (response) {
@@ -15,14 +15,18 @@
         var doc = parser.parseFromString(html, "text/html");
         
         var testcasesnip = doc.getElementById("tc_code").innerHTML;
-        testcasesnip = testcasesnip.replace(/&/g, '&amp;').replace(/</g, '&lt;')
+        // testcasesnip = testcasesnip.replace(/&/g, '&amp;').replace(/</g, '&lt;')
         //console.log("Test case snip: " + testcasesnip);
-        document.getElementById("ta_code").innerHTML = testcasesnip;
+        document.getElementById("ta_code").innerHTML = htmlEntities(testcasesnip);
     })
     //.then(y => document.getElementById("ta_code").innerHTML = testcasesnip);
     .catch(function (err) {  
         console.warn('Failed to fetch page: ', err);  
     });
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
 
 // Make the first column in each row in the Test Case Instruction table a table header
 // using scope="row" and by changing the first column td to th
