3ec878b05e9c788493b1cd5e374e30f340e7f8dc
KevinOlson-REISys <kevin.olson@reisystems.com>
2024-08-26 13:56:48 -0400

    TF07

--- testfiles/TF07/TF07.js
+++ testfiles/TF07/TF07.js
@@ -16,3 +16,29 @@ function submitForm()
         audio.play();
     }
 }
+
+function submitFormNoAudio()
+{
+    let fname = document.getElementById("fname");
+    let lname = document.getElementById("lname");
+    if((fname.value.length > 0) && (lname.value.length > 0))
+        document.getElementsByClassName("page-contents")[0].innerHTML = `<p>Thank You ${fname.value}, we will review your application shortly.</p>`;
+    else if(document.title == '07.3-1-fail-2')
+    {
+        document.getElementById("alert-notification").innerHTML = "404 Error";
+       
+    }
+
+}
+
+function validateDate() {
+    const dateField = document.getElementById("date");
+    const dateValue = dateField.value;
+    const regex = /^([0-2][0-9]|(3)[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    
+    if (!regex.test(dateValue)) {
+        alert("Please enter the date in DD/MM/YYYY format.");
+        return false;
+    }
+    return true;
+}
